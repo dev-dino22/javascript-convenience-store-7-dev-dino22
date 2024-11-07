66cb9611ab5f10f8e6a7f90b9a440ccb34e7ad8c
dev-dino22 <wlsskdud02@naver.com>
2024-11-08 00:04:16 +0900

    feat(ProductManager.js): 구매 후 재고 차감 기능

--- __tests__/ProductManagerTest.js
+++ __tests__/ProductManagerTest.js
@@ -7,10 +7,28 @@ describe('ProductManager 단위 테스트', () => {
   });
 
   test('구매하려는 상품의 재고가 충분한지 확인 (재고가 충분한 경우)', () => {
-    expect(productManager.checkProductstock('물', 10)).toBe(true);
+    expect(productManager.checkProductStock('물', 10)).toBe(true);
   });
 
   test('구매하려는 상품의 재고가 충분한지 확인 (재고가 부족한 경우)', () => {
-    expect(productManager.checkProductstock('물', 20)).toBe(false);
+    expect(productManager.checkProductStock('물', 20)).toBe(false);
+  });
+
+  test('재고가 정상적으로 차감되고, 부족 시 오류 발생', () => {
+    const productName = '물';
+    const initialStock = 10;
+    const quantityToDeduct = 1;
+
+    // 10번 차감 테스트
+    for (let i = 0; i < initialStock; i++) {
+      expect(() =>
+        productManager.deductStock(productName, quantityToDeduct),
+      ).not.toThrow();
+    }
+
+    // 11번째 차감 시 에러 발생
+    expect(() =>
+      productManager.deductStock(productName, quantityToDeduct),
+    ).toThrow('[ERROR]');
   });
 });
--- src/models/ProductManager.js
+++ src/models/ProductManager.js
@@ -7,7 +7,7 @@ class ProductManager {
     this.#products = loadProductData();
   }
 
-  checkProductstock(name, quantity) {
+  checkProductStock(name, quantity) {
     const promotionalStock = this.#products.find(
       (product) => product.name === name && product.promotion !== null,
     );
@@ -18,6 +18,33 @@ class ProductManager {
       (promotionalStock?.quantity || 0) + (regularStock?.quantity || 0);
 
     return totalStock >= quantity;
+  }
+
+  deductStock(name, quantity) {
+    if (!this.checkProductStock(name, quantity)) {
+      throw new Error('[ERROR]');
+    }
+
+    let remainingQuantity = quantity;
+
+    const promotionalStock = this.#products.find(
+      (product) => product.name === name && product.promotion !== null,
+    );
+    if (promotionalStock) {
+      const deductAmount = Math.min(
+        promotionalStock.quantity,
+        remainingQuantity,
+      );
+      promotionalStock.quantity -= deductAmount;
+      remainingQuantity -= deductAmount;
+    }
+
+    const regularStock = this.#products.find(
+      (product) => product.name === name && product.promotion === null,
+    );
+    if (remainingQuantity > 0 && regularStock) {
+      regularStock.quantity -= remainingQuantity;
+    }
   }
 }
 
