2399c82c29ff8bdaa0d17f468cf4bad2507a9c8d
dev-dino22 <wlsskdud02@naver.com>
2024-11-09 21:36:57 +0900

    feat(InputView.js): 프로모션 무료 상품 추가 여부 입력 받기 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -55,10 +55,25 @@ const InputView = {
       return this.readMembershipDiscount();
     }
   },
+
+  async readPromotionAddConfirmation(productName, additionalQuantity) {
+    const input = await Console.readLineAsync(
+      `현재 ${productName}은(는) ${additionalQuantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`,
+    );
+    const answer = input.trim().toUpperCase();
+
+    if (answer === 'Y' || answer === 'N') {
+      return answer === 'Y';
+    } else {
+      Console.print('[ERROR] Y 또는 N으로 입력해 주세요.');
+      return this.readPromotionAddConfirmation(productName, additionalQuantity);
+    }
+  },
 };
 
 await InputView.readItem();
 await InputView.readMembershipDiscount();
 await InputView.readAdditionalQuantity();
+await InputView.readPromotionAddConfirmation('콜라', 1);
 
 export default InputView;
