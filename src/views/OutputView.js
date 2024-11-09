f3b15ff449e19b2677f9fdd388d29db9203d82dc
dev-dino22 <wlsskdud02@naver.com>
2024-11-09 22:20:02 +0900

    feat(OutputView.js): 현재 상품 목록 출력

---
+++ src/views/OutputView.js
@@ -0,0 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printProducts(formattedProductsInfo) {
+    Console.print(formattedProductsInfo);
+  },
+};
+
+export default OutputView;
