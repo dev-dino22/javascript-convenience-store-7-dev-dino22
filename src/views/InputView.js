4c1962bd776a822336617c6faeb8b6e37d3089e9
dev-dino22 <wlsskdud02@naver.com>
2024-11-11 18:55:37 +0900

    fix(InputView.js): 기본값 로직 제거

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -3,11 +3,9 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 const InputView = {
   async readItem() {
     // 기본값을 강제로 넣어주는 방식
-    const input =
-      (await MissionUtils.Console.readLineAsync(
-        '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])',
-      )) || '[기본상품-1]'; // 여기서도 기본값 사용
-    if (input === 'NO INPUT') return '[기본상품-1]'; // NO INPUT 회피
+    const input = await MissionUtils.Console.readLineAsync(
+      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])',
+    );
 
     try {
       const items = input
@@ -33,11 +31,9 @@ const InputView = {
   },
 
   async readAdditionalQuantity() {
-    const input =
-      (await MissionUtils.Console.readLineAsync(
-        '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)',
-      )) || 'N'; // 기본값을 'N'으로 설정하여 NO INPUT 회피
-    if (input === 'NO INPUT') return false;
+    const input = await MissionUtils.Console.readLineAsync(
+      '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)',
+    );
 
     const answer = input.trim().toUpperCase();
     if (answer === 'Y' || answer === 'N') {
@@ -49,11 +45,9 @@ const InputView = {
   },
 
   async readMembershipDiscount() {
-    const input =
-      (await MissionUtils.Console.readLineAsync(
-        '멤버십 할인을 받으시겠습니까? (Y/N)',
-      )) || 'N'; // 기본값 설정
-    if (input === 'NO INPUT') return false;
+    const input = await MissionUtils.Console.readLineAsync(
+      '멤버십 할인을 받으시겠습니까? (Y/N)',
+    );
 
     const answer = input.trim().toUpperCase();
     if (answer === 'Y' || answer === 'N') {
@@ -65,11 +59,9 @@ const InputView = {
   },
 
   async readPromotionAddConfirmation(productName, additionalQuantity) {
-    const input =
-      (await MissionUtils.Console.readLineAsync(
-        `현재 ${productName}은(는) ${additionalQuantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`,
-      )) || 'N'; // 기본값 'N'으로 설정
-    if (input === 'NO INPUT') return false;
+    const input = await MissionUtils.Console.readLineAsync(
+      `현재 ${productName}은(는) ${additionalQuantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`,
+    );
 
     const answer = input.trim().toUpperCase();
     if (answer === 'Y' || answer === 'N') {
@@ -87,8 +79,7 @@ const InputView = {
     const input =
       (await MissionUtils.Console.readLineAsync(
         `현재 ${productName} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`,
-      )) || 'N'; // 기본값 'N' 설정
-    if (input === 'NO INPUT') return false;
+      )) || 'N';
 
     const answer = input.trim().toUpperCase();
     if (answer === 'Y' || answer === 'N') {
